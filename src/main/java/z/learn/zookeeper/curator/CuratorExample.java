5ffc7c9ed29c13d3f12100568eadc0a93a4537d9
chouyua <logsoi@126.com>
2019-02-07 15:33:05 +0800

    modify

---
+++ src/main/java/z/learn/zookeeper/curator/CuratorExample.java
@@ -0,0 +1,26 @@
+package z.learn.zookeeper.curator;
+
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.imps.CuratorFrameworkState;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
+/**
+ * 启动 zookeeper
+ * cd /home/z/workspace/kafka_2.11-1.1.0
+ * bin/zookeeper-server-start.sh config/zookeeper.properties
+ */
+public class CuratorExample {
+
+    public static void main(String[] args) throws Exception {
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        CuratorFramework client = CuratorFrameworkFactory.newClient("localhost:2181", retryPolicy);
+        client.start();
+        CuratorFrameworkState state = client.getState();
+        System.out.println(state.name());
+
+        String helloWorld = "HelloWorld";
+        client.create().forPath("/my/path", helloWorld.getBytes());
+    }
+}
